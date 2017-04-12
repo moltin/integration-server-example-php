1a30d912f8a4ee765d9332cf9f8f359d5975c248
Andrew Waters <andrew@band-x-media.co.uk>
2017-04-12 16:27:56 +0100

    Adds update and delete to integrations

--- src/app/Entities/Integration.php
+++ src/app/Entities/Integration.php
@@ -28,11 +28,57 @@ class Integration extends \IntegrationServer\Entity
 
             $email = new Email();
             $email->setSubject("Integration Updated");
-            $email->setHTML("<p>An integration has been updated</p>");
-            $email->setPlain("An integration has been updated");
-            $email->cancel();
+            $email->setHTML("<p>An integration has been created</p>");
+            $email->setPlain("An integration has been created");
             return $email->getResponseBody();
         }
     }
 
+    public function updated($type, $payload)
+    {
+        if ($type === 'webhook') {
+
+            if (($slack = $this->slack())) {
+
+                $message = 'An integration has been updated';
+                if (($integration = $this->getResource('integration', $payload))) {
+                    $forgeLink = $this->forgeLink("integrations/" . $integration->id);
+                    $message .= " (" . $integration->name . ")";
+                    $message .= "\n<" . $forgeLink . "|View on Forge>";
+                }
+
+                $slack->send($message);
+            }
+        }
+
+        if ($type === 'email') {
+
+            $email = new Email();
+            $email->setSubject("Integration Updated");
+            $email->setHTML("<p>An integration has been updated</p>");
+            $email->setPlain("An integration has been updated");
+            return $email->getResponseBody();
+        }
+    }
+
+    public function deleted($type, $payload)
+    {
+        if ($type === 'webhook') {
+
+            if (($slack = $this->slack())) {
+
+                $message = 'An integration has been deleted';
+                $slack->send($message);
+            }
+        }
+
+        if ($type === 'email') {
+
+            $email = new Email();
+            $email->setSubject("Integration Deleted");
+            $email->setHTML("<p>An integration has been deleted</p>");
+            $email->setPlain("An integration has been deleted");
+            return $email->getResponseBody();
+        }
+    }
 }
