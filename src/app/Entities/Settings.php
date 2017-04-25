c5a0a188ef2709e9bfe563954ff813c0ab1d3e52
Andrew Waters <andrew@band-x-media.co.uk>
2017-04-25 22:26:56 +0100

    Adds settings updated handlers

--- src/app/Entities/Settings.php
+++ src/app/Entities/Settings.php
@@ -26,6 +26,33 @@ class Settings extends \IntegrationServer\Entity
         if ($type === 'email') {
 
             $email = new Email();
+            $email->setSubject("Settings Created");
+            $email->setHTML("<p>Settings have been created</p>");
+            $email->setPlain("Settings have been created");
+            $email->cancel();
+            return $email->getResponseBody();
+        }
+    }
+
+    public function updated($type, $payload)
+    {
+        if ($type === 'webhook') {
+
+            if (($slack = $this->slack())) {
+
+                $message = 'Settings have been updated';
+                if (($settings = $this->getResource('settings', $payload))) {
+                    $forgeLink = $this->forgeLink("admin/settings");
+                    $message .= "\n<" . $forgeLink . "|View on Forge>";
+                }
+
+                $slack->send($message);
+            }
+        }
+
+        if ($type === 'email') {
+
+            $email = new Email();
             $email->setSubject("Settings Updated");
             $email->setHTML("<p>Settings have been updated</p>");
             $email->setPlain("Settings have been updated");
@@ -33,5 +60,4 @@ class Settings extends \IntegrationServer\Entity
             return $email->getResponseBody();
         }
     }
-
 }
